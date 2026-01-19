b2935c4ca2c9d6fda1f276ae42831c3f014148bc
haochen1008 <68853023+haochen1008@users.noreply.github.com>
2026-01-19 22:14:22 +0000

    client_app.py

--- client_app.py
+++ client_app.py
@@ -11,6 +11,10 @@ st.set_page_config(page_title="Hao Harbour | London Luxury", layout="wide")
 st.markdown("""
     <style>
     /* 1. 修复筛选房源标题颜色：强制改为白色 */
+    .st-expander {
+    background-color: #1a1c23 !important; /* 换成深黑色背景 */
+    border: 2px solid #bfa064 !important; /* 金色边框 */
+}
     .st-expanderHeader p {
         color: #ffffff !important;
         font-weight: 600 !important;
